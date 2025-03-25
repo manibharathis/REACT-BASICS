dfc8906d505ccaece182658ba771cf72f393b4fd
manibharathis <remasekar.96@gmail.com>
2025-03-25 16:08:19 +0530

    added script run

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 {
   /* <div id="grandparent">
    <div id="parent">
@@ -19,15 +21,16 @@
 </div> */
 }
 //const heading = React.createElement("h1",{id:"heading"},"Hello World");
-const divElement = React.createElement("div", { id: "grandparent" }, [
-  React.createElement("div", { id: "parent1" }, [
-    React.createElement("h1", { id: "child1" }, "I am H1"),
-    React.createElement("h1", { id: "child1" }, "I am H2"),
-  ]),
-  React.createElement("div", { id: "parent2" }, [
-    React.createElement("h1", { id: "child3" }, "I am H3"),
-    React.createElement("h1", { id: "child4" }, "I am H4"),
-  ]),
-]);
+// const divElement = React.createElement("div", { id: "grandparent" }, [
+//   React.createElement("div", { id: "parent1",key:"1" }, [
+//     React.createElement("h1", { id: "child1",key:"2" }, "I am H1"),
+//     React.createElement("h1", { id: "child1",key:"3" }, "I am H2"),
+//   ]),
+//   React.createElement("div", { id: "parent2",key:"4" }, [
+//     React.createElement("h1", { id: "child3",key:"5" }, "I am H3"),
+//     React.createElement("h1", { id: "child4",key:"6" }, "I am H4"),
+//   ]),
+//]);
+const heading = <h1 id="head">Hello React</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(divElement);
+root.render(heading);
