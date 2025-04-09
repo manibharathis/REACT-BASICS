574ee40c8ed610667369cbaed77027ba2affe82b
manibharathis <remasekar.96@gmail.com>
2025-04-09 07:43:32 +0530

    component did mount

--- components/About.js
+++ components/About.js
@@ -1,13 +1,37 @@
 import User from "./User"
 import UserClass from "./UserClass"
-const About=()=>{
+import React from "react"
+import { Component } from "react"
+class About extends Component{
+constructor(){
+    console.log("parent contructor")
+    super()
+}
+componentDidMount(){
+    console.log("parent component did mount")
+}
+render(){
+    console.log("parent render")
     return(
         <div className="about">
-            <h3>About</h3>
-            <User name={'Mani Bharathi'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
-            <UserClass name={'Mani Bharathi'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
-        </div>
+                    <h3>About</h3>
+                   
+                    <UserClass name={'Mani Bharathi 1'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
+                    <UserClass name={'Mani Bharathi 2'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
+                    <UserClass name={'Mani Bharathi 3'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
+                </div>
     )
-
 }
+}
+// const About=()=>{
+//     return(
+//         <div className="about">
+//             <h3>About</h3>
+           
+//             <UserClass name={'Mani Bharathi'} phone={'9894459170'} mail={'remasekar.96@gmail.com'} location={'chennai'}/>
+//         </div>
+//     )
+
+// }
+
 export default About
--- components/UserClass.js
+++ components/UserClass.js
@@ -3,13 +3,19 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-        count:0,
-        count1:0
+        count:0
+       
     }
+    console.log(this.props.name+"child constructor")
+  }
+  componentDidMount(){
+        console.log(this.props.name+"child component did mount")
   }
   render() {
+    console.log(this.props.name+"child render")
     const { name, location, mail, phone } = this.props;
-    const {count,count1} = this.state
+    const {count} = this.state
+   
     return (
       <div className="user-card">
         <h1>{name}</h1>
@@ -17,9 +23,16 @@ class UserClass extends React.Component {
         <h3>contact - {mail}</h3>
         <h3>phone - {phone}</h3>
         <div>{"count : "+count}</div>
-        <div>{"count 1 : "+count1}</div>
+        <button onClick={()=>{
+            this.setState({count:this.state.count+1})
+        }}>increment</button>
       </div>
     );
   }
 }
 export default UserClass;
+
+// never update state variablke directly-it will create inconsistency
+// we can use this.setstate anywhere inside class , this object will contain updated value of state variable
+// react will rerender the component on calling set state
+// if therer are multiple values in state, react will only update whats inside setstate, it will not touch other values
